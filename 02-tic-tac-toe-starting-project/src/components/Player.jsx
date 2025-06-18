4929387df41d2ccc7b2a6c4fab147b8988aea6ec
yahya <yahiamohammad740@gmail.com>
2025-06-18 16:59:15 +0300

    handle click for edit

--- 02-tic-tac-toe-starting-project/src/components/Player.jsx
+++ 02-tic-tac-toe-starting-project/src/components/Player.jsx
@@ -1,11 +1,22 @@
+import { useState } from "react";
+
 export default function Player({ name, symbol }) {
+  const [isEditing, setIsEditing] = useState(false);
+
+  function handleEditClick() {
+    setIsEditing(!isEditing);
+  }
+
+  let playerName = <span className="player-name">{name}</span>;
+  let playerNameInput = <input type="text" required />;
+
   return (
     <li>
       <span className="player">
-        <span className="player-name">{name}</span>
+        {isEditing ? playerNameInput : playerName}
         <span className="player-symbol">{symbol}</span>
       </span>
-      <button>Edit</button>
+      <button onClick={handleEditClick}>Edit</button>
     </li>
   );
 }
