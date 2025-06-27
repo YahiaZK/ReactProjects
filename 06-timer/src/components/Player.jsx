df78ac31c76f92ca7f0ef4a6d3c19d9d6e8a3e00
yahya <yahiamohammad740@gmail.com>
2025-06-27 15:47:18 +0300

    making the time initially works

--- 06-timer/src/components/Player.jsx
+++ 06-timer/src/components/Player.jsx
@@ -1,10 +1,21 @@
+import { useState, useRef } from "react";
+
 export default function Player() {
+  const plyaerName = useRef();
+
+  const [enterdPlayerName, setEnteredPlayerName] = useState(null);
+
+  function handleClick() {
+    setEnteredPlayerName(plyaerName.current.value);
+    plyaerName.current.value = "";
+  }
+
   return (
     <section id="player">
-      <h2>Welcome unknown entity</h2>
+      <h2>Welcome {enterdPlayerName ?? "unknown entity"}</h2>
       <p>
-        <input type="text" />
-        <button>Set Name</button>
+        <input ref={plyaerName} type="text" />
+        <button onClick={handleClick}>Set Name</button>
       </p>
     </section>
   );
